0730042130a6721204986f1a1d80e526deab2328
Alex Burns <alexburns@Alexs-MacBook-Air.local>
2021-11-29 21:11:39 -0800

    add random

---
+++ Random/pyramiddrawer.py
@@ -0,0 +1,13 @@
+# Pyramid Drawer
+
+i = 1
+spacing = 0
+rows = int(input('Enter how tall you want the pyramid to be in rows: '))
+print('\n')
+
+while i <= (2 * rows):
+    print((rows - spacing - 1) * " " + i * "*")
+    spacing = spacing + 1
+    i = i + 2
+
+print('\nDone')
