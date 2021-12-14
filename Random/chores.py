272c8dae5b36955fd2eabbe7edf71828806cdd74
ajburns651 <ajburns651@gmail.com>
2021-12-13 21:28:51 -0800

    update dots

--- Random/chores.py
+++ Random/chores.py
@@ -1,9 +1,9 @@
 # Tells me whether It is my turn to do chores or my sisters to
 import datetime
-current_time = datetime.datetime.now() 
+current_time = datetime.datetime.now()
 
 if current_time.day%2 == 0:
     print("It is your Sisters's day to do the chores")
 else:
     print("It is your day to do the chores")
-    
+
